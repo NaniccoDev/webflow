a686b5e5164d19e3c66c77a3cfc79b28c07b0f43
NaniccoDev <thiago.napistacom@gmail.com>
2024-05-31 18:32:43 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+﻿const express = require('express')
+const cors = require('cors')
+const axios = require('axios')
+const app = express()
+app.use(cors())
+app.get('/get', async (req, res) => {
+    const options = {
+        method: 'GET',
+        url: 'https://api.pagar.me/core/v5/orders/' + req.query.order,
+        headers: {
+            accept: 'application/json',
+            authorization: 'Basic c2tfNzI0YWNlYzkzNTJlNDczYjllMDc5NjNlMjcxMmEwYjE6'
+        }
+    }
+    const response = await axios.request(options)
+    res.send(response.data);
+});
+app.listen(3000);
