af91527e91d18f4285723422d41f2e126c33d296
NaniccoDev <thiago.napistacom@gmail.com>
2024-06-05 15:12:57 -0300

    Update app.js

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.get('/pay', async (req, res) => {
                                 pix: {
                                     expires_in: (60 * 60 * 24 * 2)
                                 },
-                                customer_editable => true
+                                customer_editable: true
                             }
                         }
                     ]
